c5c29fa39444483cabad0ecbf2f46e4b24cb7484
Graff-Ostick <rusya1998shebak@gmail.com>
2021-01-15 23:45:41 +0200

    get product stock information

--- Get/Block/HelloWorld.php
+++ Get/Block/HelloWorld.php
@@ -2,69 +2,21 @@
 namespace Test\Get\Block;
 class HelloWorld extends \Magento\Framework\View\Element\Template
 {
-    protected $_categoryCollectionFactory;
-    protected $_productRepository;
-    protected $_registry;
+    protected $_stockItemRepository;
 
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
-        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $categoryCollectionFactory,
-        \Magento\Catalog\Model\ProductRepository $productRepository,
-        \Magento\Framework\Registry $registry,
+        \Magento\CatalogInventory\Model\Stock\StockItemRepository $stockItemRepository,
         array $data = []
     )
     {
-        $this->_categoryCollectionFactory = $categoryCollectionFactory;
-        $this->_productRepository = $productRepository;
-        $this->_registry = $registry;
+        $this->_stockItemRepository = $stockItemRepository;
         parent::__construct($context, $data);
     }
 
-    /**
-     * Get category collection
-     *
-     * @param bool $isActive
-     * @param bool|int $level
-     * @param bool|string $sortBy
-     * @param bool|int $pageSize
-     * @return \Magento\Catalog\Model\ResourceModel\Category\Collection or array
-     */
-    public function getCategoryCollection($isActive = true, $level = false, $sortBy = false, $pageSize = false)
+    public function getStockItem($productId)
     {
-        $collection = $this->_categoryCollectionFactory->create();
-        $collection->addAttributeToSelect('*');
-
-        // select only active categories
-        if ($isActive) {
-            $collection->addIsActiveFilter();
-        }
-
-        // select categories of certain level
-        if ($level) {
-            $collection->addLevelFilter($level);
-        }
-
-        // sort categories by some value
-        if ($sortBy) {
-            $collection->addOrderField($sortBy);
-        }
-
-        // select certain number of categories
-        if ($pageSize) {
-            $collection->setPageSize($pageSize);
-        }
-
-        return $collection;
-    }
-
-    public function getProductById($id)
-    {
-        return $this->_productRepository->getById($id);
-    }
-
-    public function getCurrentProduct()
-    {
-        return $this->_registry->registry('current_product');
+        return $this->_stockItemRepository->get($productId);
     }
 }
 ?>
