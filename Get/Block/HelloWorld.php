cb1c109e505e673c7ea69f100f4e615ea55bb159
Graff-Ostick <rusya1998shebak@gmail.com>
2021-01-19 22:59:26 +0200

    How to Get Categories from specific Product in Magento 2

--- Get/Block/HelloWorld.php
+++ Get/Block/HelloWorld.php
@@ -2,21 +2,69 @@
 namespace Test\Get\Block;
 class HelloWorld extends \Magento\Framework\View\Element\Template
 {
-    protected $_stockItemRepository;
+    protected $_categoryCollectionFactory;
+    protected $_productRepository;
+    protected $_registry;
 
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
-        \Magento\CatalogInventory\Model\Stock\StockItemRepository $stockItemRepository,
+        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $categoryCollectionFactory,
+        \Magento\Catalog\Model\ProductRepository $productRepository,
+        \Magento\Framework\Registry $registry,
         array $data = []
     )
     {
-        $this->_stockItemRepository = $stockItemRepository;
+        $this->_categoryCollectionFactory = $categoryCollectionFactory;
+        $this->_productRepository = $productRepository;
+        $this->_registry = $registry;
         parent::__construct($context, $data);
     }
 
-    public function getStockItem($productId)
+    /**
+     * Get category collection
+     *
+     * @param bool $isActive
+     * @param bool|int $level
+     * @param bool|string $sortBy
+     * @param bool|int $pageSize
+     * @return \Magento\Catalog\Model\ResourceModel\Category\Collection or array
+     */
+    public function getCategoryCollection($isActive = true, $level = false, $sortBy = false, $pageSize = false)
     {
-        return $this->_stockItemRepository->get($productId);
+        $collection = $this->_categoryCollectionFactory->create();
+        $collection->addAttributeToSelect('*');
+
+        // select only active categories
+        if ($isActive) {
+            $collection->addIsActiveFilter();
+        }
+
+        // select categories of certain level
+        if ($level) {
+            $collection->addLevelFilter($level);
+        }
+
+        // sort categories by some value
+        if ($sortBy) {
+            $collection->addOrderField($sortBy);
+        }
+
+        // select certain number of categories
+        if ($pageSize) {
+            $collection->setPageSize($pageSize);
+        }
+
+        return $collection;
+    }
+
+    public function getProductById($id)
+    {
+        return $this->_productRepository->getById($id);
+    }
+
+    public function getCurrentProduct()
+    {
+        return $this->_registry->registry('current_product');
     }
 }
 ?>
