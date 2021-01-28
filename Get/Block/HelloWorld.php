5cff84499a2b24b0e0d53a7dcaa9c1cbf8d9e70a
Graff-Ostick <rusya1998shebak@gmail.com>
2021-01-29 00:00:33 +0200

    How to Get List Products from Catalog Rule Condition in Magento 2

--- Get/Block/HelloWorld.php
+++ Get/Block/HelloWorld.php
@@ -15,16 +15,52 @@ class HelloWorld extends \Magento\Framework\View\Element\Template
         $this->_productRepository = $productRepository;
         parent::__construct($context, $data);
     }
-    public function getProductData($id){
-        $parentByChild = $this->_catalogProductTypeConfigurable->getParentIdsByChild($id);
-        if(isset($parentByChild[0])){
-            $id = $parentByChild[0];
-        }
-        return $id;
-    }
-    public function getProductBySku($sku)
+
+    /**
+     * return product
+     * @param $id
+     */
+    public function getProductById($id)
     {
-        return $this->_productRepository->get($sku);
+        return $this->_productRepository->getById($id);
     }
+
+    public function getCatalogPriceRuleProductIds()
+    {
+        $storeManager = \Magento\Framework\App\ObjectManager::getInstance()->create(
+            '\Magento\Store\Model\StoreManagerInterface'
+        );
+        $catalogRule = \Magento\Framework\App\ObjectManager::getInstance()->create(
+            '\Magento\CatalogRule\Model\RuleFactory'
+        );
+
+        $websiteId = $storeManager->getStore()->getWebsiteId();//current Website Id
+
+        $resultProductIds = [];
+        $catalogRuleCollection = $catalogRule->create()->getCollection();
+        $catalogRuleCollection->addIsActiveFilter(1);//filter for active rules only
+        foreach ($catalogRuleCollection as $catalogRule) {
+            $productIdsAccToRule = $catalogRule->getMatchingProductIds();
+            foreach ($productIdsAccToRule as $productId => $ruleProductArray) {
+                if (!empty($ruleProductArray[$websiteId])) {
+                    $resultProductIds[$productId] = $catalogRule->getName();//name of rule
+                }
+            }
+        }
+        return $resultProductIds;
+    }
+
+    /**
+     * return list of name by all rules
+     * @param $arrData
+     */
+    public function getListNameProductFromRule($arrData){
+        $arrNames = [];
+        foreach ($arrData as $key => $value){
+            array_push($arrNames, $this->getProductById($key)->getName());
+        }
+        return $arrNames;
+    }
+
 }
 ?>
