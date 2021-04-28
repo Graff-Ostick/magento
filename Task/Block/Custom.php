cf3f2a6b21abcc43640b506c0c52f35fb0a29587
Graff-Ostick <rusya1998shebak@gmail.com>
2021-04-28 11:42:21 +0300

    add custom block in category catalog? after product price with custom currency

--- Task/Block/Custom.php
+++ Task/Block/Custom.php
@@ -2,35 +2,54 @@
 
 namespace Test\Task\Block;
 
-use \Test\Task\Helper\Data;
-use \Magento\Backend\Block\Template\Context;
-use \Magento\Catalog\Model\ProductRepository;
+use Magento\Backend\Block\Template\Context;
+use Magento\Catalog\Api\CategoryRepositoryInterface;
+use Magento\Catalog\Model\Layer\Resolver;
+use Magento\Catalog\Model\ProductRepository;
+use Magento\Framework\Data\Helper\PostHelper;
+use Test\Task\Helper\Data;
 
-
-class Custom extends \Magento\Framework\View\Element\Template
+class Custom extends \Magento\Catalog\Block\Product\ListProduct
 {
     protected $_productRepository;
 
+    protected $helper;
+
+    /**
+     * Custom constructor.
+     * @param \Magento\Catalog\Block\Product\Context $context
+     * @param PostHelper $postDataHelper
+     * @param Resolver $layerResolver
+     * @param CategoryRepositoryInterface $categoryRepository
+     * @param \Magento\Framework\Url\Helper\Data $urlHelper
+     * @param array $data
+     */
     public function __construct(
-        Context $context,
-        ProductRepository $productRepository,
         Data $helper,
+        \Magento\Catalog\Block\Product\Context $context,
+        PostHelper $postDataHelper,
+        Resolver $layerResolver,
+        CategoryRepositoryInterface $categoryRepository,
+        \Magento\Framework\Url\Helper\Data $urlHelper,
         array $data = []
-    )
-    {
-        $this->_productRepository = $productRepository;
+    ) {
         $this->helper = $helper;
-        parent::__construct($context, $data);
+        parent::__construct($context, $postDataHelper, $layerResolver, $categoryRepository, $urlHelper, $data);
     }
 
     public function getCurrencyUAH()
     {
-
         return $this->helper->getLabelCurrencyUAH();
     }
 
-    public function getProductBySku($sku)
+    public function getCurrencyRUB()
     {
-        return $this->_productRepository->get($sku);
+        return $this->helper->getLabelCurrencyRUB();
     }
+
+    public function getCurrencyEURO()
+    {
+        return $this->helper->getLabelCurrencyEURO();
+    }
+
 }
--- Task/Helper/Data.php
+++ Task/Helper/Data.php
@@ -8,24 +8,77 @@ class Data extends AbstractHelper
 {
     const TASK_ENABLED_UAH    = 'task/general/enable_uah';
     const TASK_CURRENCY_UAH   = 'task/general/currency_uah';
+    const TASK_ENABLED_RUB    = 'task/general/enable_rub';
+    const TASK_CURRENCY_RUB   = 'task/general/currency_rub';
+    const TASK_ENABLED_EURO    = 'task/general/enable_euro';
+    const TASK_CURRENCY_EURO   = 'task/general/currency_euro';
 
     public function __construct(
         \Magento\Framework\App\Helper\Context $context
     ) {
         parent::__construct($context);
     }
-    public function getEnabledUAH()
+
+    public function getEnabledCurrency($enableCurrency)
     {
-        return $this->scopeConfig->getValue(
+        $enableUAH = $this->scopeConfig->getValue(
             self::TASK_ENABLED_UAH ,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
+        $enableRUB = $this->scopeConfig->getValue(
+            self::TASK_ENABLED_RUB ,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        $enableEURO = $this->scopeConfig->getValue(
+            self::TASK_ENABLED_EURO ,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+
+        switch ($enableCurrency){
+            case 'uah':
+                return $enableUAH;
+            case 'rub':
+                return $enableRUB;
+            case 'euro':
+                return $enableEURO;
+        }
     }
     public function getLabelCurrencyUAH()
     {
-        return$this->scopeConfig->getValue(
-            self::TASK_CURRENCY_UAH,
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        );
+
+        if ($this->getEnabledCurrency('uah')){
+            return $this->scopeConfig->getValue(
+                self::TASK_CURRENCY_UAH,
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            );
+        }
+        else{
+            return NULL;
+        }
+
+    }
+    public function getLabelCurrencyRUB()
+    {
+        if ($this->getEnabledCurrency('rub')) {
+            return $this->scopeConfig->getValue(
+                self::TASK_CURRENCY_RUB,
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            );
+        }
+        else{
+            return NULL;
+        }
+    }
+    public function getLabelCurrencyEURO()
+    {
+        if ($this->getEnabledCurrency('euro')) {
+            return $this->scopeConfig->getValue(
+                self::TASK_CURRENCY_EURO,
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            );
+        }
+        else {
+            return NULL;
+        }
     }
 }
