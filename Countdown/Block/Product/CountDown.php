551e695278eda2f65b7bee05776ca260be91658e
graff <rusya1998shebak@gmail.com>
2021-06-18 18:31:49 +0300

    add functionality to show first time end promotion of discount

--- Countdown/Block/Product/CountDown.php
+++ Countdown/Block/Product/CountDown.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 namespace Test\Countdown\Block\Product;
 
 use Magento\Catalog\Model\Product;
+use Magento\CatalogRule\Model\ResourceModel\Rule;
 use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Registry;
 use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 use Magento\Framework\View\Element\Template;
 use Magento\GroupedProduct\Model\Product\Type\Grouped;
+use Magento\Store\Model\StoreManagerInterface;
 use Test\Quest\Helper\CustomData;
 
 class CountDown extends Template
@@ -28,17 +30,29 @@ class CountDown extends Template
      * @var TimezoneInterface
      */
     private $timezone;
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+    /**
+     * @var Rule
+     */
+    private $rule;
 
     public function __construct(
+        Rule $rule,
         TimezoneInterface $timezone,
         Template\Context $context,
         Registry $registry,
+        StoreManagerInterface $storeManager,
         array $data
     )
     {
         $this->registry = $registry;
         parent::__construct($context, $data);
         $this->timezone = $timezone;
+        $this->storeManager = $storeManager;
+        $this->rule = $rule;
     }
 
     /**
@@ -64,14 +78,37 @@ class CountDown extends Template
         return gmdate('d h:m:s', $left);
     }
 
-    public function timeEnd(){
+    public function timeEndSpecialPrice(){
         $specialPrice =  $this->getProduct()->getSpecialToDate();
         $timeNow = strtotime(date('Y-m-d h:m:s'));
         if (strtotime($specialPrice)>$timeNow){
-            return $specialPrice;
+            return [$specialPrice];
         }
         else{
             return null;
         }
+    }
+
+    public function timeEndCatalogPrice(){
+        $productId = $this->getProduct()->getId();
+        $date = $this->timezone->date()->getTimestamp();
+        $websiteId = $this->storeManager->getStore()->getWebsiteId();
+        $customerGroupId = $this->getCustomerGroupId();
+        $rules = $this->rule->getRulesFromProduct($date, $websiteId, $customerGroupId, $productId);
+        $firstTimeEnd = $rules[0]['to_time'];
+
+        foreach ($rules as $rule){
+            if ((int)$rule['to_time']<(int)$firstTimeEnd){
+                $firstTimeEnd = $rule['to_time'];
+            }
+        }
+
+        return [gmdate('Y-m-d h:m:s', (int)$firstTimeEnd)];
+    }
+
+    public function getFirstEndTime(){
+        $catalogTime = $this->timeEndCatalogPrice();
+        $specialTime = $this->timeEndSpecialPrice();
+        return min($catalogTime, $specialTime);
     }
 }
